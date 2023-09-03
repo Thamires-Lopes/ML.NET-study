fede2de2c883898bc1939b6ff1d45b43051cb28e
Thamires Lopes <thamiresl686@gmail.com>
2023-09-03 14:14:29 -0300

    Changing implementation.

--- ML.NET study/Program.cs
+++ ML.NET study/Program.cs
@@ -1,40 +1,43 @@
 ﻿using Microsoft.ML;
-using Microsoft.ML.Data;
 using ML.NET_study.Entities;
 
-string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "iris.data");
-string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "IrisClusteringModel.zip");
-
-var mlContext = new MLContext(seed: 0);
-
-var dataView = LoadData(mlContext, _dataPath);
-
-var model = TrainModel(mlContext, dataView);
-
-SaveModel(model, dataView, _modelPath);
-
-var predictor = mlContext.Model.CreatePredictionEngine<IrisData, ClusterPrediction>(model);
-
-IDataView LoadData(MLContext mlContext, string dataPath)
+class Program
 {
-    return mlContext.Data.LoadFromTextFile<IrisData>(dataPath, hasHeader: false, separatorChar: ',');
-}
-
-ITransformer TrainModel(MLContext mlContext, IDataView dataView)
-{
-    string featuresColumnName = "Features";
-    var pipeline = mlContext.Transforms
-        .Concatenate(featuresColumnName, "SepalLength", "SepalWidth", "PetalLength", "PetalWidth")
-        .Append(mlContext.Clustering.Trainers.KMeans(featuresColumnName, numberOfClusters: 3));
-
-   return pipeline.Fit(dataView);
-}
-
-void SaveModel(ITransformer model, IDataView dataView, string modelPath)
-{
-    using (var fileStream = new FileStream(modelPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+    static void Main(string[] args)
     {
-        mlContext.Model.Save(model, dataView.Schema, fileStream);
+        string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "iris.data");
+
+        var mlContext = new MLContext(seed: 0);
+        var dataView = LoadData(mlContext, _dataPath);
+
+        var trainTestSplit = mlContext.Data.TrainTestSplit(dataView);
+        var trainData = trainTestSplit.TrainSet;
+        var testData = trainTestSplit.TestSet;
+
+        var model = TrainModel(mlContext, trainData);
+
+        var predictions = model.Transform(testData);
+
+        var metrics = mlContext.Clustering.Evaluate(predictions);
+
+        Console.WriteLine("Metrics:");
+        Console.WriteLine(metrics.DaviesBouldinIndex);
+        Console.WriteLine(metrics.AverageDistance);
+        Console.WriteLine(metrics.NormalizedMutualInformation);
+
+        IDataView LoadData(MLContext mlContext, string dataPath)
+        {
+            return mlContext.Data.LoadFromTextFile<IrisData>(dataPath, hasHeader: false, separatorChar: ',');
+        }
+
+        ITransformer TrainModel(MLContext mlContext, IDataView trainData)
+        {
+            string featuresColumnName = "Features";
+            var pipeline = mlContext.Transforms
+                .Concatenate(featuresColumnName, "SepalLength", "SepalWidth", "PetalLength", "PetalWidth")
+                .Append(mlContext.Clustering.Trainers.KMeans(featuresColumnName, numberOfClusters: 3));
+
+            return pipeline.Fit(trainData);
+        }
     }
 }
-
