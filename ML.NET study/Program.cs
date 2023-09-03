4812ca30ee2f964c54dea21a91378738aac3ce5e
Thamires Lopes <thamiresl686@gmail.com>
2023-09-03 14:37:54 -0300

    Organizing code.

--- ML.NET study/Program.cs
+++ ML.NET study/Program.cs
@@ -3,41 +3,54 @@ using ML.NET_study.Entities;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "iris.data");
 
         var mlContext = new MLContext(seed: 0);
         var dataView = LoadData(mlContext, _dataPath);
+        var splitedData = SplitData(mlContext, dataView);
 
-        var trainTestSplit = mlContext.Data.TrainTestSplit(dataView);
+        string[] featuresColumnsNames = { "SepalLength", "SepalWidth", "PetalLength", "PetalWidth" };
+
+        var model = TrainModel(mlContext, splitedData.trainData, featuresColumnsNames, 3);
+
+        var predictions = model.Transform(splitedData.testData);
+
+        Evalute(mlContext, predictions);
+    }
+
+    private static IDataView LoadData(MLContext mlContext, string dataPath)
+    {
+        return mlContext.Data.LoadFromTextFile<IrisData>(dataPath, hasHeader: false, separatorChar: ',');
+    }
+
+    private static (IDataView trainData, IDataView testData) SplitData(MLContext mlContext, IDataView data)
+    {
+        var trainTestSplit = mlContext.Data.TrainTestSplit(data);
         var trainData = trainTestSplit.TrainSet;
         var testData = trainTestSplit.TestSet;
 
-        var model = TrainModel(mlContext, trainData);
+        return (trainData, testData);
+    }
 
-        var predictions = model.Transform(testData);
+    private static ITransformer TrainModel(MLContext mlContext, IDataView trainData, string[] features, int numberCluster)
+    {
+        string featuresColumnName = "Features";
+        var pipeline = mlContext.Transforms
+            .Concatenate(featuresColumnName, features)
+            .Append(mlContext.Clustering.Trainers.KMeans(featuresColumnName, numberOfClusters: numberCluster));
 
+        return pipeline.Fit(trainData);
+    }
+
+    private static void Evalute(MLContext mlContext, IDataView predictions)
+    {
         var metrics = mlContext.Clustering.Evaluate(predictions);
 
         Console.WriteLine("Metrics:");
-        Console.WriteLine(metrics.DaviesBouldinIndex);
-        Console.WriteLine(metrics.AverageDistance);
-        Console.WriteLine(metrics.NormalizedMutualInformation);
-
-        IDataView LoadData(MLContext mlContext, string dataPath)
-        {
-            return mlContext.Data.LoadFromTextFile<IrisData>(dataPath, hasHeader: false, separatorChar: ',');
-        }
-
-        ITransformer TrainModel(MLContext mlContext, IDataView trainData)
-        {
-            string featuresColumnName = "Features";
-            var pipeline = mlContext.Transforms
-                .Concatenate(featuresColumnName, "SepalLength", "SepalWidth", "PetalLength", "PetalWidth")
-                .Append(mlContext.Clustering.Trainers.KMeans(featuresColumnName, numberOfClusters: 3));
-
-            return pipeline.Fit(trainData);
-        }
+        Console.WriteLine($"Davies-Bouldin Index: {metrics.DaviesBouldinIndex}");
+        Console.WriteLine($"Average Distance: {metrics.AverageDistance}");
+        Console.WriteLine($"Normalized Mutual Information: {metrics.NormalizedMutualInformation}");
     }
 }
